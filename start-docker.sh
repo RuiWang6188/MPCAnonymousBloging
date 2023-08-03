836d8235e7036c143c8548471875a9affbc96f95
RUi <ruiwang6188@gmail.com>
2023-08-03 18:52:03 +0000

    update start docker script

--- start-docker.sh
+++ start-docker.sh
@@ -11,16 +11,6 @@ else
   docker run \
     -v $PWD:/root \
     -ti \
-    -p 9000:9000 \
-    -p 9100:9100 \
-    -p 9200:9200 \
-    -p 9300:9300 \
-    -p 9400:9400 \
-    -p 9500:9500 \
-    -p 9600:9600 \
-    -p 9700:9700 \
-    -p 9800:9800 \
-    -p 9900:9900 \
     --network host \
     --hostname $CONTAINER_NAME \
     --name $CONTAINER_NAME \
