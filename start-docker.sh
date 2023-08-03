420b7e88e55cff4e096abe05e4f1cccb057e6f2d
RUi <ruiwang6188@gmail.com>
2023-08-03 17:11:27 +0000

    update docker start script

--- start-docker.sh
+++ start-docker.sh
@@ -11,6 +11,16 @@ else
   docker run \
     -v $PWD:/root \
     -ti \
+    -p 8000:8000 \
+    -p 8001:8001 \
+    -p 8002:8002 \
+    -p 8003:8003 \
+    -p 8004:8004 \
+    -p 8005:8005 \
+    -p 8006:8006 \
+    -p 8007:8007 \
+    -p 8008:8008 \
+    -p 8009:8009 \
     --hostname $CONTAINER_NAME \
     --name $CONTAINER_NAME \
     $DOCKER_IMAGE
