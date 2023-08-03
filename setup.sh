6de7380dcbea31bdaa7dc9e7068f4c9687706081
RUi <ruiwang6188@gmail.com>
2023-08-03 14:23:28 +0000

    fix for docker

--- setup.sh
+++ setup.sh
@@ -3,16 +3,16 @@
 cd $HOME 
 
 # prereq
-sudo apt update
-sudo apt install -y git wget build-essential
-sudo apt install -y libssl-dev libgmp3-dev cmake liblog4cpp5-dev zlib1g-dev
+apt update
+apt install -y git wget build-essential
+apt install -y libssl-dev libgmp3-dev cmake liblog4cpp5-dev zlib1g-dev
 
 # boost installation
 wget https://boostorg.jfrog.io/artifactory/main/release/1.71.0/source/boost_1_71_0.tar.bz2
 tar -xf boost_1_71_0.tar.bz2
 cd boost_1_71_0/
 ./bootstrap.sh --prefix=/usr && ./b2 stage threading=multi link=shared
-sudo ./b2 install threading=multi link=shared
+./b2 install threading=multi link=shared
 
 # build libscapi
 cd $HOME
--- start-docker.sh
+++ start-docker.sh
@@ -9,7 +9,7 @@ if docker container inspect $CONTAINER_NAME > /dev/null 2>&1; then
   docker start -ai $CONTAINER_NAME
 else
   docker run \
-    -v $PWD:/home/Rui \
+    -v $PWD:/root \
     -ti \
     --hostname $CONTAINER_NAME \
     --name $CONTAINER_NAME \
